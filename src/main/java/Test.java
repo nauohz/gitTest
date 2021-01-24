8c5b56577d8b31723d7db45175daa260e8bc17bf
nauohz <2822799274@qq.com>
2021-01-24 16:59:16 +0800

    主干冲突

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello Wrold & Dev");
+        System.out.println("主干冲突!");
     }
 }
